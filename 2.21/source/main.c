44451a2e52d8e031868ab44a2a48ad10ce84d221
chunkal <chunkal90528@gmail.com>
2019-10-09 22:26:51 +0800

    Update 2.21

--- 2.21/source/main.c
+++ 2.21/source/main.c
@@ -11,5 +11,7 @@ int main(void) {
 	printf("*       *    *       *      *         *   *\n");
 	printf("*       *     *     *       *          * *\n");
 	printf("*********       ***         *           *\n");
+
+	system("pause");
 	return 0;
 }
